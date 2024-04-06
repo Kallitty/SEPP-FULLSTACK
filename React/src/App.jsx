fcc16c07b8264769a5311a1bc5291ea78e4bd0ea
Kall <samueluche432@gmail.com>
2024-04-06 21:58:59 +0100

    new file:   .editorconfig 	new file:   .env.example 	new file:   .gitattributes 	new file:   .gitignore 	new file:   README.md 	new file:   React/.eslintrc.cjs 	new file:   React/.gitignore 	new file:   React/README.md 	new file:   React/index.html 	new file:   React/package-lock.json 	new file:   React/package.json 	new file:   React/public/vite.svg 	new file:   React/src/App.css 	new file:   React/src/App.jsx 	new file:   React/src/component/DefaultLayout.jsx 	new file:   React/src/component/GuestLayout.jsx 	new file:   React/src/index.css 	new file:   React/src/main.jsx 	new file:   React/src/router.jsx 	new file:   React/src/views/Dashboard.jsx 	new file:   React/src/views/Login.jsx 	new file:   React/src/views/NotFound.jsx 	new file:   React/src/views/Signup.jsx

---
+++ React/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div className="app">APP</div>
+        </>
+    );
+}
+
+export default App;
