a20c70895fafb75ece26603bcc6e8b0ec6996992
Kall <samueluche432@gmail.com>
2024-04-08 12:25:37 +0100

    modified:   src/views/Signup.jsx

--- React/src/views/Signup.jsx
+++ React/src/views/Signup.jsx
@@ -4,19 +4,33 @@ import { Link } from "react-router-dom";
 
 export default function Signup() {
     const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmationRef = useRef();
 
     const onSubmit = (e) => {
         e.preventDefault();
+        const payload = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
+        };
     };
     return (
         <div className="login-signup-form animated fadeInDown">
             <div className="form">
                 <form action="#" onSubmit={onSubmit}>
                     <h1 className="title">Signup for free.</h1>
-                    <input type="text" placeholder="Full name" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
+                    <input ref={nameRef} type="text" placeholder="Full name" />
+                    <input ref={emailRef} type="email" placeholder="Email" />
                     <input
+                        ref={passwordRef}
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <input
+                        ref={passwordConfirmationRef}
                         type="password"
                         placeholder="Password Confirmation"
                     />
