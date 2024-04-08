bb605c3f3761036703ee3a55538eb413920103c6
Kall <samueluche432@gmail.com>
2024-04-08 01:51:50 +0100

    modified:   src/views/Login.jsx

--- React/src/views/Login.jsx
+++ React/src/views/Login.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Login() {
-    return <div>Login</div>;
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+    return (
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form action="#" onSubmit={onSubmit}>
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <button className="btn btn-block"></button>
+                </form>
+            </div>
+        </div>
+    );
 }
