fc229892d897b0657f861c880ed5850c8b8a93ec
Kall <samueluche432@gmail.com>
2024-04-08 13:04:27 +0100

    modified:   src/views/Signup.jsx

--- React/src/views/Signup.jsx
+++ React/src/views/Signup.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { useRef } from "react";
 import { Link } from "react-router-dom";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function Signup() {
     const nameRef = useRef();
     const emailRef = useRef();
     const passwordRef = useRef();
     const passwordConfirmationRef = useRef();
+    const { setUser, setToken } = useStateContext();
 
     const onSubmit = (e) => {
         e.preventDefault();
@@ -16,7 +19,18 @@ export default function Signup() {
             password: passwordRef.current.value,
             password_confirmation: passwordConfirmationRef.current.value,
         };
-        console.log(payload);
+        axiosClient
+            .post("/signup", payload)
+            .then(({ data }) => {
+                setUser(data.user);
+                setToken(data.token);
+            })
+            .catch((err) => {
+                const response = err.response;
+                if (response && response.status === 422) {
+                    console.log(response.data.errors);
+                }
+            });
     };
     return (
         <div className="login-signup-form animated fadeInDown">
