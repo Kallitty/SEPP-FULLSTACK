a332e0183748d6e7db0f4dc9f512981a04584b92
Kall <samueluche432@gmail.com>
2024-04-08 04:02:08 +0100

    modified:   src/views/Signup.jsx

--- React/src/views/Signup.jsx
+++ React/src/views/Signup.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 
 export default function Signup() {
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
     return (
         <div className="login-signup-form animated fadeInDown">
             <div className="form">
                 <form action="#" onSubmit={onSubmit}>
-                    <h1 className="title">Login to your account.</h1>
+                    <h1 className="title">Signup for free.</h1>
                     <input type="email" placeholder="Email" />
                     <input type="password" placeholder="Password" />
-                    <button className="btn btn-block">Login</button>
+                    <button className="btn btn-block">Signup</button>
                     <p className="message">
-                        Not Registered?{" "}
-                        <Link to="/signup">Create an account</Link>
+                        Already have an account?{" "}
+                        <Link to="/login">Login to continue.</Link>
                     </p>
                 </form>
             </div>
