cd5cd9d196124fada4fb0d98fd803848853f3023
Kall <samueluche432@gmail.com>
2024-04-11 15:09:55 +0100

    modified:   React/src/views/Login.jsx 	modified:   React/src/views/Signup.jsx

--- React/src/views/Login.jsx
+++ React/src/views/Login.jsx
@@ -8,7 +8,7 @@ export default function Login() {
     return (
         <div className="login-signup-form animated fadeInDown">
             <div className="form">
-                <form action="#" onSubmit={onSubmit}>
+                <form onSubmit={onSubmit}>
                     <h1 className="title">Login to your account.</h1>
                     <input type="email" placeholder="Email" />
                     <input type="password" placeholder="Password" />
--- React/src/views/Signup.jsx
+++ React/src/views/Signup.jsx
@@ -35,7 +35,7 @@ export default function Signup() {
     return (
         <div className="login-signup-form animated fadeInDown">
             <div className="form">
-                <form action="#" onSubmit={onSubmit}>
+                <form onSubmit={onSubmit}>
                     <h1 className="title">Signup for free.</h1>
                     <input ref={nameRef} type="text" placeholder="Full name" />
                     <input ref={emailRef} type="email" placeholder="Email" />
