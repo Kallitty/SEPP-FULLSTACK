64e4bea87f654b5943925a69aa3a7bf628dd70e4
Kall <samueluche432@gmail.com>
2024-04-13 15:27:26 +0100

    modified:   React/src/views/Signup.jsx

--- React/src/views/Signup.jsx
+++ React/src/views/Signup.jsx
@@ -10,7 +10,7 @@ export default function Signup() {
     const emailRef = useRef();
     const passwordRef = useRef();
     const passwordConfirmationRef = useRef();
-    const [errors, setErrors] = useState("null");
+    const [errors, setErrors] = useState("");
     const { setUser, setToken } = useStateContext();
 
     const onSubmit = (e) => {
@@ -33,7 +33,7 @@ export default function Signup() {
             .catch((err) => {
                 const response = err.response;
                 if (response && response.status === 422) {
-                    console.log(response.data.errors);
+                    setErrors(response.data.errors);
                 }
             });
     };
@@ -42,6 +42,13 @@ export default function Signup() {
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="title">Signup for free.</h1>
+                    {errors && (
+                        <div className="alert">
+                            {Object.keys(errors).map((key) => (
+                                <p key={key}>{errors[key][0]}</p>
+                            ))}
+                        </div>
+                    )}
                     <input ref={nameRef} placeholder="Full name" />
                     <input ref={emailRef} type="email" placeholder="Email" />
                     <input
